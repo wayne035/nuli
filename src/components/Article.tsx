2debe208c78ad29e75c06f614a17234e5ba493cb
wayne <QWE24367705@gmail.com>
2024-02-28 14:37:44 +0800

    article UI 修改

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -12,10 +12,10 @@ export default function Article({data}:{data: JobData[]}) {
   return (
     <>
       {data?.map((job: JobData)=>(
-        <article key={crypto.randomUUID()} className='w-full flex justify-center flex-wrap'>
-          <a href={job.link} target='_blank' className='block border-2 bg-[#fff] border-[#000] rounded-md my-8 mx-5 md:mx-20 md:my-6 p-4 md:py-8 font-black relative hover:bg-[#ffe3fd] shadow-md hover:shadow-lg shadow-[#777] hover:shadow-[#555] w-[1024px]'>
-            <span className='absolute top-[-20px] left-[-15px] px-3 py-1 border-2 border-[#000]' style={{background: job.color}}>
-              {job.name}
+        <article key={crypto.randomUUID()} className='w-full flex justify-center flex-wrap relative'>
+          <a href={job.link} id='job-link' target='_blank' className='block border-2 bg-[#fff] border-[#000] rounded-md my-8 mx-5 md:mx-20 md:my-6 p-4 md:py-9 font-black relative shadow-md hover:shadow-lg shadow-[#777] hover:shadow-[#555] w-[1024px] overflow-hidden'>
+            <span className='absolute top-0 left-[-10px] px-3 py-1 z-10 rounded-b-md' style={{background: job.color}}>
+              {job.name + '人力銀行'}
             </span>
             <h2 className='text-[22px] text-[#b25252]'>
               {job.title}
@@ -25,6 +25,7 @@ export default function Article({data}:{data: JobData[]}) {
               <address className=' leading-[26px]'>{job.address}</address>
             </div>
             <p className='md:mt-3 text-[#8c44ab]'>{job.content}</p>
+            <span className="cover">立即應徵...👆</span>
           </a>
         </article>
       ))}
